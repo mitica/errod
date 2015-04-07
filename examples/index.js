4254288ca1437fdb308c2c5d38173a0b0fc9fd87
Dumitru Cantea <canteadumitru@gmail.com>
2015-04-07 15:07:01 +0300

    util.inherits

--- examples/index.js
+++ examples/index.js
@@ -1,27 +1,40 @@
+var assert = require('assert');
 var errors = require('../lib/errors');
 
-var FatalError = errors.create({
-  name: 'FatalError',
-  message: 'Fatal',
+var BaseError = errors.create({
+  log: function() {
+    console.log('error:', this.message);
+  },
+  name: 'BaseError',
+  message: 'Base',
   statusCode: 500
+});
+
+var FatalError = errors.create({
+  parent: errors.BaseError,
+  name: 'FatalError',
+  message: 'Fatal'
 });
 
 var UnknownError = errors.create({
   parent: FatalError,
   name: 'UnknownError',
   message: 'Unknown error',
-  explanation: 'Some problems'
+  details: 'Some problems'
 });
 
 var fatal = new FatalError('Super error');
+
+assert(fatal instanceof Error);
 
 var unknown = new UnknownError({
   statusCode: 401,
   message: 'Test error'
 });
 
+unknown.log();
 
-console.log(fatal, fatal.stack);
+console.log(fatal);
 
 try {
   throw unknown;
